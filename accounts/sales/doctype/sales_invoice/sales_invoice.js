aa4062326b2192ca0d403a3ef06003df577de461
hasnain2808 <hasnain2808@gmail.com>
2020-08-26 18:58:00 +0530

    Feat: change total amount when fields change

--- accounts/sales/doctype/sales_invoice/sales_invoice.js
+++ accounts/sales/doctype/sales_invoice/sales_invoice.js
@@ -5,7 +5,7 @@ frappe.ui.form.on('Sales Invoice', {
 	refresh: function(frm) {
 		console.log("inside refresh event");
 		frm.add_custom_button(__('Create Payment Entry'),function() {
-			var method = "accounts.sales.doctype.sales_invoice.sales_invoice.get_payment_entry";
+			let method = "accounts.sales.doctype.sales_invoice.sales_invoice.get_payment_entry";
 			return frappe.call({
 				method: method,
 				args: {
@@ -13,7 +13,7 @@ frappe.ui.form.on('Sales Invoice', {
 					"dn": frm.doc.name
 				},
 				callback: function(r) {
-					var doclist = frappe.model.sync(r.message);
+					let doclist = frappe.model.sync(r.message);
 					frappe.set_route("Form", doclist[0].doctype, doclist[0].name);
 				}
 			});
@@ -24,15 +24,18 @@ frappe.ui.form.on('Sales Invoice', {
 
 
 frappe.ui.form.on("Sales Invoice Item", {
-	qty: function (frm, cdt, cdn) {
-		var cur_doc = locals[cdt][cdn];
-		cur_doc.amount = cur_doc.qty * cur_doc.buying_price;
-		var sum = 0
-		for (var row in locals[cdt]) {
-			console.log(typeof (row))
-			sum += locals[cdt][row].amount;
-		}
-		frm.set_value("total_amount", sum)
-		frm.refresh_fields();
-	},
+	qty : update_total_amount,
+	selling_price : update_total_amount,
 })
+
+function update_total_amount(frm, cdt, cdn){
+	let cur_doc = locals[cdt][cdn];
+	cur_doc.amount = cur_doc.qty * cur_doc.selling_price;
+	let sum = 0
+	for (let row in locals[cdt]) {
+		if (! isNaN(locals[cdt][row].amount)) {
+			sum+=locals[cdt][row].amount;
+		}	}
+	frm.set_value("total_amount", sum)
+	frm.refresh_fields();
+}
