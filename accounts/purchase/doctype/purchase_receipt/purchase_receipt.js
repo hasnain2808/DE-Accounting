b2067a41f6a9489bde4509f28a808903057e2a4d
hasnain2808 <hasnain2808@gmail.com>
2020-08-31 14:26:09 +0530

    Select Accounts in Purchase cycle

--- accounts/purchase/doctype/purchase_receipt/purchase_receipt.js
+++ accounts/purchase/doctype/purchase_receipt/purchase_receipt.js
@@ -5,52 +5,63 @@ frappe.ui.form.on('Purchase Receipt', {
 	// refresh: function(frm) {
 
 	// },
-	refresh: function(frm) {
-		console.log("inside refresh event");
-		frm.add_custom_button(__('Create Purchase Invoice'),function() {
+	refresh: function (frm) {
+		frm.add_custom_button(__('Create Purchase Invoice'), function () {
 			frappe.model.open_mapped_doc({
 				method: "accounts.purchase.doctype.purchase_receipt.purchase_receipt.make_purchase_invoice",
 				frm: frm
 			})
 		})
 	},
+	company: function (frm) {
+		frm.set_value("debit_account", "Stock in Hand - " + frm.doc.company)
+		frm.set_value("credit_account", "Asset Received But not Billed - " + frm.doc.company)
+		frm.refresh_fields();
+		set_debit_account_filter(frm)
+		set_credit_account_filter(frm)
+	},
 });
 
+function set_debit_account_filter(frm) {
+	frm.set_query("debit_account", function () {
+		return {
+			"filters": {
+				"company_name": frm.doc.company,
+				"parent_account": "Stock Assets - " + frm.doc.company
+
+			}
+		};
+	});
+}
+
+function set_credit_account_filter(frm) {
+	frm.set_query("debit_account", function () {
+		return {
+			"filters": {
+				"company_name": frm.doc.company,
+				"parent_account": "Stock Liabilities - " + frm.doc.company
+			}
+		};
+	});
+}
 
 frappe.ui.form.on("Purchase Receipt Items", {
 	qty: update_total_amount,
 	buying_price: update_total_amount,
 })
 
-// function update_total_amount(frm, cdt, cdn){
-// 	let cur_doc = locals[cdt][cdn];
-// 	cur_doc.amount = cur_doc.qty * cur_doc.buying_price;
-// 	let sum = 0
-// 	for (let row in locals[cdt]) {
-// 		if (! isNaN(locals[cdt][row].amount)) {
-// 			sum+=locals[cdt][row].amount;
-// 		}
-// 	}
-// 	frm.set_value("total_amount", sum)
-// 	frm.refresh_fields();
-// }
-
-
-function update_total_amount(frm, cdt, cdn){
+function update_total_amount(frm, cdt, cdn) {
 	let cur_doc = locals[cdt][cdn];
-	// console.log(frm.doc.product_list)
 	cur_doc.amount = cur_doc.qty * cur_doc.buying_price;
 	let sum = 0.0
-	if (! isNaN(frm.doc) ) {
-		frm.doc.product_list.forEach(element => {
-			console.log(element)
-			if (!( isNaN(element) && isNaN(element.amount))) {
-				sum+=flt(element.amount);
-				console.log(sum)
-			}
-		});
-	}
+	frm.doc.product_list.forEach(element => {
+		console.log(element)
+		if (!(isNaN(element) && isNaN(element.amount))) {
+			sum += flt(element.amount);
+			console.log(sum)
+		}
+	});
+	console.log(sum)
 	frm.set_value("total_amount", sum)
 	frm.refresh_fields();
 }
-
